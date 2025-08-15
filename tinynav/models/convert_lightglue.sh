dcd23dcfe6932a7e825f0bf74607635b89338ffb
YANG Zhenfei <zhenfei.yang@deepmirror.com>
2025-08-15 22:29:51 +0800

    fix model names in convert scripts (#98)
    Co-authored-by: dvorak <yangzhenfei0@gmail.com>

--- tinynav/models/convert_lightglue.sh
+++ tinynav/models/convert_lightglue.sh
@@ -1,4 +1,4 @@
-ONNX_MODEL=./lightglue_v1.onnx
+ONNX_MODEL=./lightglue_fp16.onnx
 OUTPUT_MODEL=$(basename $ONNX_MODEL .onnx)_$(uname -m).plan
 echo "Converting $ONNX_MODEL to $OUTPUT_MODEL"
 trtexec --onnx=$ONNX_MODEL \
--- tinynav/models/convert_superpoint.sh
+++ tinynav/models/convert_superpoint.sh
@@ -1,4 +1,4 @@
-ONNX_MODEL=./superpoint.onnx
+ONNX_MODEL=./superpoint_240x424_fp16.onnx
 OUTPUT_MODEL=$(basename $ONNX_MODEL .onnx)_$(uname -m).plan
 echo "Converting $ONNX_MODEL to $OUTPUT_MODEL"
 trtexec --onnx=$ONNX_MODEL \
